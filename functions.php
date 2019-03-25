5094c588bbc2cebcbae0da70dc23eb0d91e693e6
Peter <peterlanier@me.com>
2019-03-25 15:44:54 -0600

    enqueued and registered styles and scripts

--- functions.php
+++ functions.php
@@ -1,5 +1,29 @@
 <?php
 
-//LOAD PHP
+// LOAD PHP
 include("dashboard_customizer.php");
 // include("post_types.php");
+
+// LOAD CSS
+function theme_styles() {
+	wp_register_style('child-theme', get_stylesheet_directory_uri() . '/style.css');
+
+	wp_enqueue_style('child-theme');
+	
+}
+
+// LOAD JS
+function theme_scripts() {
+	wp_register_script('scripts', get_stylesheet_directory_uri() . '/scripts.js', array('jquery'), false, true);
+	
+	wp_enqueue_script('scripts');
+	
+}
+
+// CHECK FOR THEME OR DASHBOARD
+if ( ! is_admin() ) {
+     add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+     add_action( 'wp_enqueue_scripts', 'theme_styles' );
+} else {
+
+}
---
+++ scripts.js
@@ -0,0 +1,1 @@
+console.log("jupiter child theme ready!");
